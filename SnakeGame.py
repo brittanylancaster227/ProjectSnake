d41996b7405bf0ba8420b5a9431b7578120bbeb6
lanevillec <lanevillec@gmail.com>
2022-05-13 13:43:19 -0400

    updates from ADO repo

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,5 +1,5 @@
 import pygame
-from Snake import Snake
+import SnakeSegment as ss
 
 #initializing pygame functions
 pygame.init()
@@ -16,20 +16,73 @@ window = pygame.display.set_mode(window_size)
 pygame.display.set_caption('Snake Game')
 
 #good to use a group of sprites for flexibility to add more later
-all_sprites_list = pygame.sprite.Group()
+#snake = pygame.sprite.Group()
+snake = []
+ss_group = pygame.sprite.Group()
 
-#create an instance of our snake class and specify its location
-snake_sprite = Snake(GREEN, 100,10,window_x,window_y,'right')
-snake_sprite.rect.x = 0
-snake_sprite.rect.y = window_y - snake_sprite.rect.height
-#add instance of snake to our sprite list
-all_sprites_list.add(snake_sprite)
+#create snake segments!
+size = 20
+initial_pos_x = 180
+initial_pos_y = window_y - size
+initial_direction = 'right'
+k = 1
+while k <= 10:
+    name = 'ss' + str(k)
+    snake.append(ss.SnakeSegment(name, BLACK, size, window_x, window_y, initial_pos_x, initial_pos_y, initial_direction))
+    #increment loop variables
+    initial_pos_x -= size
+    k += 1
+
+for ss in snake:
+    index = snake.index(ss)
+    if index == 0:
+        ss.segment_behind = snake[index+1]
+    elif index == 9:
+        ss.segment_in_front = snake[index-1]
+    else:
+        ss.segment_behind = snake[index+1]
+        ss.segment_in_front = snake[index-1]
+    ss_group.add(ss)
+
 #clocks are fun
 fps = pygame.time.Clock()
+'''
+def show_game_over_screen():
+    window.blit(background, background_rect)
+    draw_text(window, "SHMUP!", 64, WIDTH / 2, HEIGHT / 4)
+    pygame.display.flip()
+    waiting = True
+    while waiting:
+        fps.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYUP:
+                waiting = False
+'''
 
 #start to the game functionality
 game_running = True
 while game_running:
+
+    game_over = False
+    #HANDLE GAME OVER
+    print('*************************************')
+    count = 1
+    print('WHILE LOOP # ' + str(count))
+    for ss in ss_group:
+        print('------------------------')
+        for other_ss in ss_group:
+            print('comparing ' + ss.name + ' to ' + other_ss.name + ', ' + str(ss.rect.x) + ' to ' + str(other_ss.rect.x) + ', and ' + str(ss.rect.y) + ' to ' + str(other_ss.rect.y))
+            if(ss.name is not other_ss.name and ss.rect.x is other_ss.rect.x and ss.rect.y is other_ss.rect.y):
+                game_over = True
+                print('******************SS HAVE CRASHED!!!!!')
+
+    if(game_over):
+        game_over = True
+    #if game_over:
+        #Pop up a screen saying you lose
+        #show_game_over_screen()
     #good form to put the escape first
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -38,25 +91,28 @@ while game_running:
     #get pressed :)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        snake_sprite.direction = 'left'
+        snake[0].direction = 'left'
     if keys[pygame.K_RIGHT]:
-        snake_sprite.direction = 'right'
+        snake[0].direction = 'right'
     if keys[pygame.K_UP]:
-        snake_sprite.direction = 'up'
+        snake[0].direction = 'up'
     if keys[pygame.K_DOWN]:
-        snake_sprite.direction = 'down'
+        snake[0].direction = 'down'
 
-    all_sprites_list.update()
+    #snake.update()
+    for ss in ss_group:
+        ss.update()
 
     window.fill(BLACK)
 
-    all_sprites_list.draw(window)
+    ss_group.draw(window)
+    
 
     #SUPER IMPORTANT, FLIP REFRESHES THE SCREEN
     pygame.display.flip()
 
     #fps
-    fps.tick(60)
+    fps.tick(30)
 
 #End Game
 pygame.QUIT()
--- SnakeSegment.py
+++ SnakeSegment.py
@@ -5,15 +5,18 @@ WHITE = (255, 255, 255)
 
 class SnakeSegment(pygame.sprite.Sprite):
 
-    def __init__(self, color, size, window_x, window_y, initial_x, initial_y, direction):
+    def __init__(self, name, color, size, window_x, window_y, initial_x, initial_y, direction):
         
         super().__init__()
-        
+        self.name = name
+        self.color = color
+        self.size = size
         self.image = pygame.image.load('smileyFaceResized.png')
         self.rect = self.image.get_rect()
         #self.image = pygame.Surface([size, size])
         #self.image.fill(WHITE)
         #self.image.set_colorkey(WHITE)
+        self.segment_should_move = True
         self.window_x = window_x
         self.window_y = window_y
         self.direction = direction
@@ -39,27 +42,80 @@ class SnakeSegment(pygame.sprite.Sprite):
     def moveDown(self, pixels):
         self.rect.y += pixels
     """
+
+    def is_at_right_edge(self):
+        if(self.rect.x + self.rect.width >= self.window_x):
+            return True
+        else:
+            return False
+    
+    def is_at_left_edge(self):
+        if(self.rect.x <= 0):
+            return True
+        else:
+            return False
+    
+    def is_at_top_edge(self):
+        if(self.rect.y <= 0):
+            return True
+        else:
+            return False
+    
+    def is_at_bottom_edge(self):
+        if(self.rect.y + self.rect.height >= self.window_y):
+            return True
+        else:
+            return False
+
     def update(self):
-        #leader
-        #print('I AM AT ' + '(' + str(self.rect.x) + ', ' + str(self.rect.y) + ')')
-        #print('MY PREVIOUS POSITION WAS ' + '(' + str(self.prev_x) + ', ' + str(self.prev_y) + ')')
+        #USED FOR DEBUGGING POSITIONS
+        '''
+        leader
+        print('------------')
+        if(self.segment_in_front is None):
+            print('----------------------------------')
+            print('I AM THE LEADER!')
+        print('I AM AT ' + '(' + str(self.rect.x) + ', ' + str(self.rect.y) + ')')
+        print('MY PREVIOUS POSITION WAS ' + '(' + str(self.prev_x) + ', ' + str(self.prev_y) + ')')
+        print('I SHOULD MOVE NOW?: ' + self.segment_should_move)
+        print()
+        '''
+        
         self.prev_x = self.rect.x
         self.prev_y = self.rect.y
+
         if self.segment_in_front is None:
             match self.direction:
                 case 'right':
-                    if(self.rect.x + self.rect.width < self.window_x):
-                        self.rect.x += 20
+                    if(not self.is_at_right_edge()):
+                        self.segment_should_move = True
+                        self.rect.x += self.size
+                    else:
+                        self.segment_should_move = False
                 case 'left':
-                    if(self.rect.x > 0):
-                        self.rect.x -= 20
+                    if(not self.is_at_left_edge()):
+                        self.segment_should_move = True
+                        self.rect.x -= self.size
+                    else:
+                        self.segment_should_move = False
                 case 'up':
-                    if(self.rect.y > 0):
-                        self.rect.y -= 20
+                    if(not self.is_at_top_edge()):
+                        self.segment_should_move = True
+                        self.rect.y -= self.size
+                    else:
+                        self.segment_should_move = False
                 case 'down':
-                    if(self.rect.y + self.rect.height < self.window_y):
-                        self.rect.y += 20
+                    if(not self.is_at_bottom_edge()):
+                        self.segment_should_move = True
+                        self.rect.y += self.size
+                    else:
+                        self.segment_should_move = False
         #segments behind
         else:
-            self.rect.x = self.segment_in_front.prev_x  
-            self.rect.y = self.segment_in_front.prev_y
+            #determine if segment in front moved last tick. if it didn't, don't move the segment
+            if(self.segment_in_front.segment_should_move):
+                self.segment_should_move = True
+                self.rect.x = self.segment_in_front.prev_x  
+                self.rect.y = self.segment_in_front.prev_y
+            else:
+                self.segment_should_move = False
